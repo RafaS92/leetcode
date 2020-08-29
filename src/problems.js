f630f93db68cc446ba54baa99c8565c655bcbbf7
RafaS92 <manrvaldez..92@gmail.com>
2020-08-29 13:41:06 -0500

    9 palindrome number

---
+++ src/problems.js
@@ -0,0 +1,21 @@
+//27 Remove Element
+function removeElement(nums, val) {
+  if (!nums) return 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+}
+
+//9 Palindrome number
+function isPalindrome(x) {
+  if (x < 0) return false;
+  let reverse = 0;
+
+  for (let i = x; i >= 1; i = Math.floor(i / 10))
+    reverse = reverse * 10 + (i % 10);
+  return reverse === x;
+}
