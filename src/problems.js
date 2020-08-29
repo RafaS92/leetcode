caab71dc8bd70da597efd8b75055aa7f16b084d1
RafaS92 <manrvaldez..92@gmail.com>
2020-08-29 15:00:29 -0500

    problem 7

--- src/problems.js
+++ src/problems.js
@@ -32,3 +32,10 @@ function runningSum(nums) {
   }
   return total;
 }
+
+//7 Reverse Integer
+var reverse = function (x) {
+  if (x < 0) return -1 * reverse(-x);
+  const solution = x.toString().split("").reverse().join("");
+  return solution > 2 ** 31 - 1 ? 0 : solution;
+};
