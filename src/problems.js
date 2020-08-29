aa1759b1f3349c601ac7211a86f9f35db268f9b0
RafaS92 <manrvaldez..92@gmail.com>
2020-08-29 15:48:09 -0500

    problem 1431

--- src/problems.js
+++ src/problems.js
@@ -39,3 +39,15 @@ var reverse = function (x) {
   const solution = x.toString().split("").reverse().join("");
   return solution > 2 ** 31 - 1 ? 0 : solution;
 };
+
+//1431 Kids Wtith the Greatest Number of Candies
+function kidsWithCandies(candies, extraCandies) {
+  const max = Math.max(...candies);
+  let result = [];
+
+  for (let i = 0; i < candies.length; i++) {
+    let cv = candies[i];
+    result.push(cv + extraCandies >= max);
+  }
+  return result;
+}
