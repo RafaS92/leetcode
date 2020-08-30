1df478bf80a4e9083e41b8f45710d886af037dd6
RafaS92 <manrvaldez..92@gmail.com>
2020-08-30 11:01:26 -0500

    problem 1342

--- src/problems.js
+++ src/problems.js
@@ -98,3 +98,20 @@ var smallerNumbersThanCurrent = function (nums) {
 
   return result;
 };
+
+//1342 Number of Steps to Reduce a Number to Zero
+var numberOfSteps = function (num) {
+  let count = 0;
+  if (num < 0) return 0;
+
+  while (num > 0) {
+    if (num % 2 == 0) {
+      num /= 2;
+    } else {
+      num -= 1;
+    }
+    count++;
+  }
+
+  return count;
+};
