169ce687be5a8e284f76ed1b427016de8b8f4156
RafaS92 <manrvaldez..92@gmail.com>
2020-08-30 09:15:00 -0500

    problem 1365

--- src/problems.js
+++ src/problems.js
@@ -76,3 +76,25 @@ var defangIPaddr = function (address) {
 
   return result;
 };
+
+//1365 How Many Numbers Are Smaller Than the Current Number
+
+var smallerNumbersThanCurrent = function (nums) {
+  let result = [];
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    cn = nums[i];
+
+    for (let j = 0; j < nums.length; j++) {
+      let num = nums[j];
+
+      if (cn > num) count++;
+    }
+
+    result.push(count);
+    count = 0;
+  }
+
+  return result;
+};
