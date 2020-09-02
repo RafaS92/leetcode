16bb4f5c7cb0e23b6db34fc04482c3d1f4f4da23
RafaS92 <manrvaldez..92@gmail.com>
2020-09-02 16:44:40 -0500

    problem 1528

--- src/problems.js
+++ src/problems.js
@@ -128,3 +128,15 @@ var numJewelsInStones = function (J, S) {
 
   return count;
 };
+
+//1528
+
+var restoreString = function (s, indices) {
+  let result = [];
+
+  for (let i = 0; i < s.length; i++) {
+    result[indices[i]] = s[i];
+  }
+
+  return result.join("");
+};
