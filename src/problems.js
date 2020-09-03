3b80bd1e27999fb631bd1cb0f214cba98eb13378
RafaS92 <manrvaldez..92@gmail.com>
2020-09-03 16:12:19 -0500

    problem 1389

--- src/problems.js
+++ src/problems.js
@@ -208,3 +208,15 @@ var countNegatives = function (grid) {
   });
   return counter;
 };
+
+//1389
+
+var createTargetArray = function (nums, index) {
+  let target = [];
+
+  for (let i = 0; i < index.length; i++) {
+    target.splice(index[i], 0, nums[i]);
+  }
+
+  return target;
+};
