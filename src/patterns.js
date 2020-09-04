4d1bbfb8ee7d888d3e6250885134940e742b7ee3
RafaS92 <manrvaldez..92@gmail.com>
2020-09-04 12:16:47 -0500

    Sliding window

--- src/patterns.js
+++ src/patterns.js
@@ -31,3 +31,23 @@ function sumZero(arr) {
     }
   }
 }
+
+//Sliding Windodw
+
+function maxSubarraysSum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+
+  if (arr.length < num) return null;
+  for (let i = num; i < arr.length; i++) {
+    maxSum += arr[i];
+  }
+
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+
+  return maxSum;
+}
