ba88393510bfeace725ef2ec87edaa5aefab09ed
RafaS92 <manrvaldez..92@gmail.com>
2020-09-04 09:19:59 -0500

    Multiple pointer patter 1

---
+++ src/patterns.js
@@ -0,0 +1,16 @@
+//Multiple pointers Pattern
+
+//Going from left
+
+function countUniquevalues(arr) {
+  var i = 0;
+  if (arr.length === 0) return 0;
+  for (var j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+
+  return i + 1;
+}
