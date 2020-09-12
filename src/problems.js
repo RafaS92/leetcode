4bc49299831a2f2504903859c55573d432aa353d
RafaS92 <manrvaldez..92@gmail.com>
2020-09-12 18:21:30 -0500

    1309

--- src/problems.js
+++ src/problems.js
@@ -369,3 +369,18 @@ var uniqueOccurrences = function (arr) {
 
   return true;
 };
+
+//1309
+
+var freqAlphabets = function (s) {
+  let result = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i + 2] === "#") {
+      result += String.fromCharCode(96 + Number(s[i] + s[i + 1]));
+      i++;
+    } else if (s[i] !== "#") {
+      result += String.fromCharCode(96 + Number(s[i]));
+    }
+  }
+  return result;
+};
